efba031ac8a4b5a4d96a004f8c0728f33ddc2f5d
Gia <61707471+GiaJordan@users.noreply.github.com>
2021-03-13 17:08:04 -0700

    Evaluation Metrics
    Data also added

--- LSTM.py
+++ LSTM.py
@@ -2,23 +2,39 @@
 """
 Created on Tue Mar  9 17:05:34 2021
 
-@author: GuestUser
 """
+    
+
+#GPU Config ########################################################
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+#GPU Config ########################################################
+
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, auc, roc_auc_score, roc_curve,percision_score, recall_score
+from sklearn.metrics import accuracy_score, auc, roc_auc_score, roc_curve, precision_score, recall_score
+from scipy import interp
 
 import pandas as pd
 
 
 # import testing and training sets
-x_train         = pd.read_csv('C:/Users/GuestUser/Desktop/spring 2021/BME 578/Week 7 & 8/UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt',header=None, delim_whitespace=True)
-y_train_uncoded = pd.read_csv('C:/Users/GuestUser/Desktop/spring 2021/BME 578/Week 7 & 8/UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt',header=None, delim_whitespace=True)
-x_test          = pd.read_csv('C:/Users/GuestUser/Desktop/spring 2021/BME 578/Week 7 & 8/UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt',header=None, delim_whitespace=True)
-y_test_uncoded  = pd.read_csv('C:/Users/GuestUser/Desktop/spring 2021/BME 578/Week 7 & 8/UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt',header=None, delim_whitespace=True)
+#x_train         = pd.read_csv('C:/Users/GuestUser/Desktop/spring 2021/BME 578/Week 7 & 8/UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt',header=None, delim_whitespace=True)
+#y_train_uncoded = pd.read_csv('C:/Users/GuestUser/Desktop/spring 2021/BME 578/Week 7 & 8/UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt',header=None, delim_whitespace=True)
+#x_test          = pd.read_csv('C:/Users/GuestUser/Desktop/spring 2021/BME 578/Week 7 & 8/UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt',header=None, delim_whitespace=True)
+#y_test_uncoded  = pd.read_csv('C:/Users/GuestUser/Desktop/spring 2021/BME 578/Week 7 & 8/UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt',header=None, delim_whitespace=True)
+
+x_train         = pd.read_csv('./Data/UCI HAR Dataset/train/X_train.txt',header=None, delim_whitespace=True)
+y_train_uncoded = pd.read_csv('./Data/UCI HAR Dataset/train/y_train.txt',header=None, delim_whitespace=True)
+x_test          = pd.read_csv('./Data/UCI HAR Dataset/test/X_test.txt',header=None, delim_whitespace=True)
+y_test_uncoded  = pd.read_csv('./Data/UCI HAR Dataset/test/y_test.txt',header=None, delim_whitespace=True)
+
+
+
 
 #create dummy variables of labels
 y_train= pd.get_dummies(y_train_uncoded[0])
@@ -46,7 +62,63 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 model.fit(x_train, y_train, epochs=15, batch_size=50)
 
 # predict the testing dataset
-y_predicted= model.predict(x_test)
-y_predicted=np.around(y_predicted,0)
+y_predicted = model.predict(x_test)
+y_predicted = np.around(y_predicted,0)
 
+y_predicted=y_predicted.astype(np.uint8)
+
+
+#Evaluation and Metrics
+
+y_score=model.predict(x_test)
+
+acc=accuracy_score(y_test,y_predicted)   
+precision=precision_score(y_test,y_predicted,average='micro')
+recall=recall_score(y_test,y_predicted,average='micro')
+
+
+falsePosRate=dict()
+truePosRate=dict()
+rAUC=dict()
+
+for i in range(6):
+    falsePosRate[i], truePosRate[i], _ = roc_curve(y_test[:, i],y_score[:, i])
+    rAUC[i]=auc(falsePosRate[i],truePosRate[i])
+
+falsePosRate["micro"],truePosRate["micro"],_=roc_curve(y_test.ravel(),y_score.ravel())
+rAUC["micro"]=auc(falsePosRate["micro"],truePosRate["micro"])
+
+allFalsePos=np.unique(np.concatenate([falsePosRate[i] for i in range(6)]))
+
+meanTruePos=np.zeros_like(allFalsePos)
+for i in range(6):
+    meanTruePos += interp(allFalsePos,falsePosRate[i],truePosRate[i])\
+        
+        
+meanTruePos/=6
+
+falsePosRate["macro"]=allFalsePos
+truePosRate["macro"]=meanTruePos
+rAUC["macro"]=auc(falsePosRate["macro"],truePosRate["macro"])
+
+plt.figure()
+plt.plot(falsePosRate["micro"],truePosRate["micro"])
+plt.title("Micro-average ROC")
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.show()
+    
+plt.figure()
+plt.plot(falsePosRate["macro"],truePosRate["macro"])
+plt.title("Macro-average ROC")
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.show()
+
+
+
+print("Micro AUC:",rAUC["micro"])
+print("Macro AUC:",rAUC["macro"])
+print("Accuracy:",acc)
+print("Precision:",precision,"Recall:",recall)
 
